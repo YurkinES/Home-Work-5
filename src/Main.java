ad89c6f7846823c4eb419d025a23a1e78db3a157
YurkinES <6eJlomop@inbox.ru>
2023-02-21 23:25:06 +0300

    Stage 2

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@ public class Main {
     public static void main(String[] args) {
     //Task #1-2
     byte clientOS = 1;
-    short releaseYear = 2015;
-    if (clientOS == 0 && releaseYear >= 2015 ) {
+    byte iOS = 0;
+    byte androidOS = 1;
+    short clientDeviceYear = 2015;
+    if (clientOS == iOS && clientDeviceYear >= 2015 ) {
         System.out.println("Установите версию приложения для iOS по ссылке");
-    } else if (clientOS == 0 && releaseYear < 2015) {
+    } else if (clientOS == iOS && clientDeviceYear < 2015) {
         System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-    if (clientOS == 1 && releaseYear >= 2015) {
+    if (clientOS == androidOS && clientDeviceYear >= 2015) {
         System.out.println("Установите версию приложения для Android по ссылке");
-    } else if (clientOS == 1 && releaseYear < 2015) {
+    } else if (clientOS == androidOS && clientDeviceYear < 2015) {
         System.out.println("Установите облегченную версию приложения для Android по ссылке");
     }
+    //Task #3
+    int year = 2021;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        System.out.println(year + " год является високосным");
+    } else { System.out.println(year + " год не является високосным");}
     }
 }
